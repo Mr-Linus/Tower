f11a485b666dd4d7a3bff8b012821d112b94c45b
Funky <mrfunky001@gmail.com>
2019-08-31 20:50:00 +0800

    update frontend code

--- Dashboard/lists.py
+++ Dashboard/lists.py
@@ -18,7 +18,29 @@ class K8S:
     def job_num(self):
         return len(client.BatchV1Api().list_job_for_all_namespaces().items)
 
+    def job_list(self):
+        return client.BatchV1Api().list_job_for_all_namespaces().items
+
+    def pod_list(self):
+        return client.CoreV1Api().list_pod_for_all_namespaces(watch=False).items
+
+    def node_list(self):
+        return client.CoreV1Api().list_node(watch=False).items
+
+    def get_pod_name_with_job_uid(self, job_uid):
+        for pod in client.CoreV1Api().list_pod_for_all_namespaces(watch=False).items:
+            if job_uid == pod.metadata.owner_references[0].uid:
+                return pod.metadata.name
+
+    def get_pod_name_with_job_name(self, job_name, job_namespace):
+        job_uid = client.BatchV1Api().read_namespaced_job_status(job_name, job_namespace).metadata.uid
+        for pod in client.CoreV1Api().list_pod_for_all_namespaces(watch=False).items:
+            if job_uid == pod.metadata.owner_references[0].uid:
+                return pod.metadata.name
+        return None
+
+
 
 if __name__ == '__main__':
     k = K8S()
-    print(k.pod_num(), k.node_num(), k.namespace_num(), k.job_num())
+    print(k.node_list())
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -11,8 +11,5 @@ class DashboardIndexView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         k = lists.K8S()
         context = super().get_context_data(**kwargs)
-        context['node_num'] = k.node_num()
-        context['pod_num'] = k.pod_num()
-        context['namespace_num'] = k.namespace_num()
-        context['job_num'] = k.job_num()
+        context['k'] = k
         return context
