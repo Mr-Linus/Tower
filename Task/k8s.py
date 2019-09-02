1d673b8c45e042f1b72e69c8ccdc6d78e2b6f6d4
Funky <mrfunky001@gmail.com>
2019-09-02 14:48:42 +0800

    add namespace func

--- Task/k8s.py
+++ Task/k8s.py
@@ -2,17 +2,41 @@ from kubernetes import client, config
 
 
 class K8sTask:
+    namespace = ''
+
+    user = ''
 
     def __init__(self):
         config.load_kube_config()
 
     def get_namespace(self):
-        return client.CoreV1Api().list_namespace().items
+        namespace = []
+        for ns in client.CoreV1Api().list_namespace().items:
+            if ns.metadata.labels is not None:
+                if {"user": self.user} == ns.metadata.labels:
+                    namespace.append(ns.metadata.name)
+        return namespace
+
+    def find_namespace(self):
+        namespace = []
+        for ns in client.CoreV1Api().list_namespace().items:
+            if self.namespace in ns.metadata.name:
+                namespace.append(ns)
+        return namespace
 
     def create_namespace(self):
-        return client.CoreV1Api().create_namespace()
+        info = client.V1Namespace(
+            api_version="v1",
+            kind="Namespace",
+            metadata=client.V1ObjectMeta(name=self.namespace, labels={"user": self.user})
+        )
+        return client.CoreV1Api().create_namespace(body=info)
+
+    def delete_namespace(self):
+        return client.CoreV1Api().delete_namespace(self.namespace)
 
 
 if __name__ == '__main__':
     k = K8sTask()
+    k.user = 'test'
     print(k.get_namespace())
