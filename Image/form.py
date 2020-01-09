8d25b488288a1becf952824327b4df9389395de9
mr-linus <mrfunky001@gmail.com>
2020-01-09 20:45:46 +0800

    add func image

---
+++ Image/form.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+class CreatePullForm(forms.Form):
+    imageName = forms.CharField(
+        max_length=20,
+        label="镜像名称",
+        help_text="容器镜像的名称，例如：helloworld:latest",
+        required=True
+    )
--- Image/urls.py
+++ Image/urls.py
@@ -3,5 +3,6 @@ from Image import views
 
 app_name = 'Image'
 urlpatterns = [
-    path('nodes/list', views.NodeListView.as_view()),
+    path('', views.ListView.as_view()),
+    path('pull', views.PullView.as_view()),
 ]
